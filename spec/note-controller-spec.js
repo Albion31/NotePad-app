3326876c0a2866d1da3f1161b4b1f39ca88bedab
Lan Pham <lanpham312@gmail.com>
2018-01-12 12:14:33 +0000

    adding a test for note list with 2 notes

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -14,7 +14,7 @@ window.onload = function(){
 
     var DoubleView = function(){
       DoubleView.prototype.returnHTML = function(){
-        return '<ul><li><div>Favourite Drink: Spiced rum and Ginger beer</div></li></ul>';
+        return '<ul><li><div>Favourite Drink: Spiced rum and coke</div></li></ul>';
       };
     };
 
@@ -26,4 +26,25 @@ window.onload = function(){
     assert.isTrue(document.getElementById('app').innerHTML === html, 'InnerHTML should contain a note list with one note ')
   };
   test_thirteen();
+
+  function test_fourteen(){
+
+    var DoubleNoteList = function(){
+    };
+
+    var DoubleView = function(){
+      DoubleView.prototype.returnHTML = function(){
+        return '<ul><li><div>Favourite Drink: Spiced rum and Ginger beer</div></li><li><div>Favourite Drink: JD and coke</div></li></ul>';
+
+      };
+    };
+
+    var notelist = new DoubleNoteList();
+    var view = new DoubleView();
+    var controller = new Controller(notelist, view);
+    var html = controller.view.returnHTML();
+    controller.getHTML('app')
+    assert.isTrue(document.getElementById('app').innerHTML === html, 'InnerHTML should contain a note list with two note ')
+  };
+  test_fourteen();
 };
